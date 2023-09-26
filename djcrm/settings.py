acb10152af4d9bf5fcf7eb2baef93d8655668a5a
ArmanAghania <Arman94.vergil94@gmail.com>
2023-09-26 04:32:29 +0330

    minor changes

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -6,7 +6,7 @@ import dj_database_url
 from dotenv import load_dotenv
 import locale
 
-locale.setlocale(locale.LC_ALL, "fa_IR.UTF-8")
+locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
 load_dotenv()
 
